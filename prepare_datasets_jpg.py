29675c792017d678b76e7fb0910ed8c6e7b433c5
Sheikh Shah Mohammad Motiur Rahman <motiur.ion@gmail.com>
2022-10-27 11:27:04 +0200

    -- update the number of samples

--- prepare_datasets_jpg.py
+++ prepare_datasets_jpg.py
@@ -29,9 +29,9 @@ if __name__ == "__main__":
     #for v in range(len(mask_file_list)):
     select_random = []
     if purpose == 'train':
-        select_random = random.choices(mask_file_list, k=10)
+        select_random = random.choices(mask_file_list, k=25)
     elif purpose == 'test':
-        select_random = random.choices(mask_file_list, k=2)
+        select_random = random.choices(mask_file_list, k=8)
     for v in select_random:
         if im_type == 'jpg' and noise_type == 'gaussian':
             #file_name =  mask_file_list[v]
